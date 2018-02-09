756898458cb5c442c09f6d7b20ee08693e41385d
Jared Bangs <jared@agilestory.com>
2018-02-08 23:25:11 -0800

    show headers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.send(req.headers);
 });
 
 app.listen(port, function () {
