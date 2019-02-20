745dbc169cf1ef3cf7e9e2c7a979bcb6bdb916e1
Jared Bangs <jaredbangs@gmail.com>
2019-02-19 22:24:08 -0800

    Revert

--- middleware/user.js
+++ middleware/user.js
@@ -10,13 +10,8 @@ module.exports = function (req, res, next) {
     req.userId = process.env.USER_OVERRIDE || 'Jared (local dev)';
 
     if (req.header('x-ms-client-principal-name') !== undefined) {
-        req.userName = req.header('x-ms-client-principal-name');
+        req.userId = req.header('x-ms-client-principal-name');
     }
-    
-    if (req.header('x-ms-client-principal-id') !== undefined) {
-        req.userId = req.header('x-ms-client-principal-id');
-    }
-
 
     userDataRepository.withUserModel(req.userId, function (err, userModel) {
 
